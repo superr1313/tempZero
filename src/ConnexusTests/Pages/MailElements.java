d19ed7e5bf8b26f9023c5adfefe27cf05c69952b
Randa Nunn <randa.nunn@mokimobility.com>
2014-09-18 22:04:13 -0600

    code format cleanup

--- src/ConnexusTests/Pages/MailElements.java
+++ src/ConnexusTests/Pages/MailElements.java
@@ -1,12 +1,11 @@
 package ConnexusTests.Pages;
 
-import javax.swing.JOptionPane;
-
+import ConnexusTests.CommonElements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import ConnexusTests.CommonElements;
+import javax.swing.*;
 
 public class MailElements extends CommonElements {
     public MailElements(WebDriver driver) 
@@ -17,28 +16,22 @@ public class MailElements extends CommonElements {
     /**
      * Identify elements on the page.
      */
-    public WebElement getNextButton()
-    {
+    public WebElement getNextButton() {
         return source.findElement(By.id("ctl01"));
     }
-    public WebElement getStudentCheckbox()
-    {
+    public WebElement getStudentCheckbox() {
         return source.findElement(By.id("students"));
     }
-    public WebElement getCaretakerCheckbox()
-    {
+    public WebElement getCaretakerCheckbox() {
         return source.findElement(By.id("caretakersAndLearningCoaches"));
     }
-    public WebElement getTeacherCheckbox()
-    {
+    public WebElement getTeacherCheckbox() {
         return source.findElement(By.id("teachers"));
     }
-    public WebElement getEmailIFrame()
-    {
+    public WebElement getEmailIFrame() {
         return source.findElement(By.id("body_editor_contentIframe"));
     }
-    public WebElement getSendButton()
-    {
+    public WebElement getSendButton() {
         return source.findElement(By.id("send"));
     }
     
@@ -47,62 +40,54 @@ public class MailElements extends CommonElements {
      * @throws InterruptedException 
      */
 
-    public void setClassEmails()
-    {
-        for(int index : displaySectionOptions())
-        {
+    public void setClassEmails() {
+        for(int index : displaySectionOptions()) {
             String emailText = JOptionPane.showInputDialog ( "Enter email to send to " + classList.get(index).className + " : " + classList.get(index).classSchool);
             classList.get(index).classEmailText = emailText;
         }
     }
-    public void clickNextButton()
-    {
+
+    public void clickNextButton() {
         getNextButton().click();
     }
-    public void setRecipientTypeCheckbox(int recipients)
-    {
-        if(recipients == 2)
-        {
+
+    public void setRecipientTypeCheckbox(int recipients) {
+        if(recipients == 2) {
             getStudentCheckbox().click();
             getCaretakerCheckbox().click();
         }
-        else if(recipients == 3)
-        {
+        else if(recipients == 3) {
             getStudentCheckbox().click();
             getTeacherCheckbox().click();
         }
-        else if(recipients == 4)
-        {
+        else if(recipients == 4) {
             getCaretakerCheckbox().click();
         }
-        else if(recipients == 5)
-        {
+        else if(recipients == 5) {
             getTeacherCheckbox().click();
         }
-        else if(recipients == 6)
-        {
+        else if(recipients == 6) {
             getStudentCheckbox().click();
             getCaretakerCheckbox().click();
             getTeacherCheckbox().click();
         }
-        else if(recipients == 7)
-        {
+        else if(recipients == 7) {
             getCaretakerCheckbox().click();
             getTeacherCheckbox().click();
         }
     }
-    public void enterEmailText(String emailText)
-    {
+
+    public void enterEmailText(String emailText) {
         source.switchTo().frame(getEmailIFrame());
         WebElement editable = source.switchTo().activeElement(); 
         editable.sendKeys(emailText); 
     }
-    public void clickSend()
-    {
+
+    public void clickSend() {
         getSendButton().click();
     }
-    public void sendClassEmails(String emailText, int recipients) throws InterruptedException
-    {
+
+    public void sendClassEmails(String emailText, int recipients) throws InterruptedException {
         setRecipientTypeCheckbox(recipients);
         clickNextButton();
         waitForElementToShow(getEmailIFrame());
@@ -113,14 +98,12 @@ public class MailElements extends CommonElements {
         //then add a waitForElementToShow(thatElement()); here
         //then i think this is done!!
     }
-    public void sendEmailToEachSection(int index, int emailRecipients) throws InterruptedException
-    {
+
+    public void sendEmailToEachSection(int index, int emailRecipients) throws InterruptedException {
         getClassList(index);
         setClassEmails();
-        for(int i = 0; i<classList.size(); i++)
-        {
-            if(classList.get(i).classEmailText != null)
-                {
+        for(int i = 0; i<classList.size(); i++) {
+            if(classList.get(i).classEmailText != null) {
                     System.out.println(classList.get(i).classURL);
                     source.get(classList.get(i).classURL);
                     waitForElementToShow(getNextButton());
